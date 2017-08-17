402eb235109d10782a8c86d8bcf4dcc5e7dbd8cf
greatjing <greatjing@sina.cn>
2017-08-17 14:49:58 +0800

    add short-term test

--- spec/models/parking_spec.rb
+++ spec/models/parking_spec.rb
@@ -20,40 +20,83 @@ RSpec.describe Parking, type: :model do
   end
 
   describe ".calculate_amount" do
-    it "30 mins should be ¥2" do
-      t = Time.now
-      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 30.minutes )
-      parking.calculate_amount
-      expect(parking.amount).to eq(200)
+    # 一般用户的计费校验
+    context "guest" do
+      it "30 mins should be ¥2" do
+        t = Time.now
+        parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 30.minutes )
+        parking.calculate_amount
+        expect(parking.amount).to eq(200)
+      end
+
+      it " 60 mins should be ¥2 " do
+        t = Time.now
+        parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 60.minutes )
+        parking.calculate_amount
+        expect(parking.amount).to eq(200)
+      end
+
+      it "61 mins should be ¥3" do
+        t = Time.now
+        parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 61.minutes )
+        parking.calculate_amount
+        expect(parking.amount).to eq(300)
+      end
+
+      it "90 mins should be ¥3" do
+        t = Time.now
+        parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 90.minutes )
+        parking.calculate_amount
+        expect(parking.amount).to eq(300)
+      end
+
+      it "120 mins should be ¥4" do
+        t = Time.now
+        parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 120.minutes )
+        parking.calculate_amount
+        expect(parking.amount).to eq(400)
+      end
     end
 
-    it " 60 mins should be ¥2 " do
-      t = Time.now
-      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 60.minutes )
-      parking.calculate_amount
-      expect(parking.amount).to eq(200)
+    # 短期费率
+    context "short-term" do
+      it " 30 mins should be ¥2 " do
+        t =  Time.now
+        parking = Parking.new( :parking_type => "short-term", :start_at => t, :end_at => t + 30.minutes )
+        parking.user = User.create(:email => "test@examples.com", :password => "123456" )
+        parking.calculate_amount
+        expect(parking.amount).to eq(200)
+      end
+
+      it " 60 mins should be 2 " do
+        t = Time.now
+        parking = Parking.new( :parking_type => "short-term", :start_at => t, :end_at => t + 60.minutes )
+        parking.user = User.new(:email => "test@examples.com", :password => "123456")
+      end
+
+      it "61 mins should be ¥3" do
+        t = Time.now
+        parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 61.minutes )
+        parking.calculate_amount
+        expect(parking.amount).to eq(250)
+      end
+
+      it "90 mins should be ¥3" do
+        t = Time.now
+        parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 90.minutes )
+        parking.calculate_amount
+        expect(parking.amount).to eq(250)
+      end
+
+      it "120 mins should be ¥4" do
+        t = Time.now
+        parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 120.minutes )
+        parking.calculate_amount
+        expect(parking.amount).to eq(300)
+      end
+
     end
 
-    it "61 mins should be ¥3" do
-      t = Time.now
-      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 61.minutes )
-      parking.calculate_amount
-      expect(parking.amount).to eq(300)
-    end
-
-    it "90 mins should be ¥3" do
-      t = Time.now
-      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 90.minutes )
-      parking.calculate_amount
-      expect(parking.amount).to eq(300)
-    end
-
-    it "120 mins should be ¥4" do
-      t = Time.now
-      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 120.minutes )
-      parking.calculate_amount
-      expect(parking.amount).to eq(400)
-    end
   end
 
 end
